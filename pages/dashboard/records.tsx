a6e65247958baef15d6203be6b4a720eae29e213
Guillermo Beltran <bego@codiks.com>
2021-06-18 20:44:14 -0500

    sort by date

--- pages/dashboard/records.tsx
+++ pages/dashboard/records.tsx
@@ -14,9 +14,14 @@ export type Record = {
 };
 
 function Records({ records }: any) {
+  const sortedRecords = records.sort((r1, r2) => {
+    const t1 = new Date(r1.timestamp);
+    const t2 = new Date(r2.timestamp);
+    return t2 - t1;
+  });
   return (
     <DashboardLayout>
-      <Table items={records} />
+      <Table items={sortedRecords} />
     </DashboardLayout>
   );
 }
